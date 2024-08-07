d3e12ede0b020466af211dae24d76f1c07d9a717
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2024-08-07 21:42:51 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -1,41 +1,30 @@
+import os
+import logging
 import requests
 from bs4 import BeautifulSoup
 
+logging.basicConfig(level=logging.INFO)
 
-url_list = {}
-api_key = "ENTER YOUR API KEY HERE"
+MOVIE_SEARCH_URL = os.environ.get('MOVIE_SEARCH_URL', 'https://example.com/search')
 
+def search_movie(movie_name):
+    url = f"{MOVIE_SEARCH_URL}?q={movie_name}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Add your movie search logic here
+    return []
 
-def search_movies(query):
-    movies_list = []
-    movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://mkvcinemas.skin/?s={query.replace(' ', '+')}").text, "html.parser")
-    movies = website.find_all("a", {'class': 'ml-mask jt'})
+def get_movie_download_link(movie_url):
+    # Add your movie download link logic here
+    return ""
+
+def main():
+    movie_name = "The Shawshank Redemption"  # Replace with user input
+    movies = search_movie(movie_name)
     for movie in movies:
-        if movie:
-            movies_details["id"] = f"link{movies.index(movie)}"
-            movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
-            url_list[movies_details["id"]] = movie['href']
-        movies_list.append(movies_details)
-        movies_details = {}
-    return movies_list
+        download_link = get_movie_download_link(movie['url'])
+        # Add your movie download logic here
+        print(f"Downloading {movie['title']}...")
 
-
-def get_movie(query):
-    movie_details = {}
-    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
-    if movie_page_link:
-        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
-        movie_details["title"] = title
-        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
-        movie_details["img"] = img
-        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
-        final_links = {}
-        for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-            response = requests.get(url)
-            link = response.json()
-            final_links[f"{i.text}"] = link['shortenedUrl']
-        movie_details["links"] = final_links
-    return movie_details
-
+if __name__ == '__main__':
+    main()
