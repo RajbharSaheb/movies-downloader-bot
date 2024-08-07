453ffbffb06ae2e3ad3d2f8e80c793e7e36460cb
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2024-08-07 21:40:43 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,85 +1,34 @@
 import os
-from io import BytesIO
-from queue import Queue
-import requests
-from flask import Flask, request
-from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
-from movies_scraper import search_movies, get_movie
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.utils.request import Request
 
+logging.basicConfig(level=logging.INFO)
 
-TOKEN = os.getenv("TOKEN")
-URL = os.getenv("URL")
-bot = Bot(TOKEN)
+TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a movie downloader bot.")
 
-def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
-                              f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
-    update.message.reply_text("👇 Enter Movie Name 👇")
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Just send me a movie name, and I'll try to download it for you!")
 
+def download_movie(update, context):
+    movie_name = update.message.text
+    # Add your movie download logic here
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Downloading {movie_name}...")
 
-def find_movie(update, context):
-    search_results = update.message.reply_text("Processing...")
-    query = update.message.text
-    movies_list = search_movies(query)
-    if movies_list:
-        keyboards = []
-        for movie in movies_list:
-            keyboard = InlineKeyboardButton(movie["title"], callback_data=movie["id"])
-            keyboards.append([keyboard])
-        reply_markup = InlineKeyboardMarkup(keyboards)
-        search_results.edit_text('Search Results...', reply_markup=reply_markup)
-    else:
-        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
+def main():
+    updater = Updater(TOKEN, use_context=True)
 
+    dp = updater.dispatcher
 
-def movie_result(update, context) -> None:
-    query = update.callback_query
-    s = get_movie(query.data)
-    response = requests.get(s["img"])
-    img = BytesIO(response.content)
-    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
-    link = ""
-    links = s["links"]
-    for i in links:
-        link += "🎬" + i + "\n" + links[i] + "\n\n"
-    caption = f"⚡ Fast Download Links :-\n\n{link}"
-    if len(caption) > 4095:
-        for x in range(0, len(caption), 4095):
-            query.message.reply_text(text=caption[x:x+4095])
-    else:
-        query.message.reply_text(text=caption)
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(MessageHandler(Filters.text, download_movie))
 
+    updater.start_polling()
+    updater.idle()
 
-def setup():
-    update_queue = Queue()
-    dispatcher = Dispatcher(bot, update_queue, use_context=True)
-    dispatcher.add_handler(CommandHandler('start', welcome))
-    dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
-    dispatcher.add_handler(CallbackQueryHandler(movie_result))
-    return dispatcher
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-
-@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
-def respond():
-    update = Update.de_json(request.get_json(force=True), bot)
-    setup().process_update(update)
-    return 'ok'
-
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+if __name__ == '__main__':
+    main()
